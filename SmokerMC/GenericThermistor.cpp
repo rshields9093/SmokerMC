875aa86a9b715d845605e3aba4d3eb305506e0c9
rober <rober@RKS-M3800>
2017-03-04 10:07:46 -0500

    Created GenericThermistor Class

---
+++ SmokerMC/GenericThermistor.cpp
@@ -0,0 +1,18 @@
+/*
+ * GenericThermistor.cpp
+ *
+ *  Created on: Mar 4, 2017
+ *      Author: rober
+ */
+
+#include "GenericThermistor.h"
+
+GenericThermistor::GenericThermistor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GenericThermistor::~GenericThermistor() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ SmokerMC/GenericThermistor.h
@@ -0,0 +1,17 @@
+/*
+ * GenericThermistor.h
+ *
+ *  Created on: Mar 4, 2017
+ *      Author: rober
+ */
+
+#ifndef GENERICTHERMISTOR_H_
+#define GENERICTHERMISTOR_H_
+
+class GenericThermistor {
+public:
+	GenericThermistor();
+	virtual ~GenericThermistor();
+};
+
+#endif /* GENERICTHERMISTOR_H_ */
