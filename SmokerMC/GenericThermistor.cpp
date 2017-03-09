402467dee428d4c3d42329f214d560dad5907a7a
rober <rober@RKS-M3800>
2017-03-09 13:59:25 -0500

    Added Beta and Steinhart-Hart calculations

--- SmokerMC/GenericThermistor.cpp
+++ SmokerMC/GenericThermistor.cpp
@@ -6,52 +6,85 @@
  */
 
 #include "GenericThermistor.h"
-//#include <math.h>
+#include <math.h>
 #include "Arduino.h"
 
-GenericThermistor::GenericThermistor(int pin, int beta, unsigned long seriesRes, unsigned long thermistorNominal, int numSamples) {
-	useBeta = true;
+GenericThermistor::GenericThermistor(int pin, int beta, unsigned long seriesRes,
+		unsigned long thermistorNominalRes, double thermistorNominalTemp, bool isPullUp, int numSamples) {
+	this->useBeta = true;
 	this->pin = pin; //analog pin the thermistor is on
 	this->beta = beta; //Beta coefficient of the thermistor
 	this->seriesRes = seriesRes; //value of series resitor in voltage divider
+	this->thermistorNominalResistance = thermistorNominalRes; //the nominal resistance of the thermistor
+	this->thermistorNominalTemperature = thermistorNominalTemp; //the temperature at which the thermistor's nominal resistance is reported, usually 25C.
+	this->isPullUp = isPullUp; //whether or not the thermistor is tied to Vcc (othereise it is pull-down configuration)
 	this->numSamples = numSamples; //for data smoothing, thermistor value will be read this many times and result averaged, default = 5
 }
 
-GenericThermistor::GenericThermistor(int pin, float a, float b, float c, unsigned long seriesRes, int numSamples) {
+GenericThermistor::GenericThermistor(int pin, double a, double b, double c, unsigned long seriesRes,
+		bool isPullUp, int numSamples) {
 	this->useBeta = false;
 	this->pin = pin; //analog pin the thermistor is on
 	this->coeff_A = a; //Steinhart-Hart coefficient A
 	this->coeff_B = b; //Steinhart-Hart coefficient B
 	this->coeff_C = c; //Steinhart-Hart coefficient C
 	this->seriesRes = seriesRes; //value of series resitor in voltage divider
+	this->isPullUp = isPullUp; //whether or not the thermistor is tied to Vcc (othereise it is pull-down configuration)
 	this->numSamples = numSamples; //for data smoothing, thermistor value will be read this many times and result averaged, default = 5
 }
 
 //Calculate temperature using the Beta coefficient method
 double GenericThermistor::getTempWithBeta(int ADC_Value) {
 	double temp;
-	//TODO Calculate temp from ADC using Beta
+
+	// convert the value to resistance
+	ADC_Value = 1023 / ADC_Value - 1;
+	ADC_Value = this->seriesRes / ADC_Value;
+
+	temp = ADC_Value / this->thermistorNominalResistance;     // (R/Ro)
+	temp = log(temp); // ln(R/Ro)
+	temp /= this->beta; // 1/B * ln(R/Ro)
+	temp += 1.0 / (this->thermistorNominalTemperature + 273.15); // + (1/To)
+
+	if( ! this->isPullUp) {temp = 1.0 / temp;} //invert -- we would NOT do this if the thermistor was connected to Vcc instead of GND
+
 	return temp;
 }
 
 //Calculate temperature using the full Steinhart-Hart equation with 3 coefficients
 double GenericThermistor::getTempWithSteinhartHart(int ADC_Value) {
 	double temp;
-	//TODO Calculate temp from ADC using full Steinhart-Hart
+
+	if(this->isPullUp) {
+		temp = log(this->seriesRes / ((1024.0 / ADC_Value - 1))); // for pull-up configuration
+	} else {
+		temp = log(this->seriesRes * ((1024.0 / ADC_Value - 1))); // for pull-down configuration
+	}
+
+	temp = 1 / (this->coeff_A + (this->coeff_B + (this->coeff_C * temp * temp ))* temp );
+
 	return temp;
 }
 
 //Return temperature in Kelvin
 double GenericThermistor::getTempK() const {
 	double tempK;
+	int samples[this->numSamples];
+
+	for (int i = 0; i < this->numSamples; i++) {
+		samples[i] = analogRead(this->pin);
+		delay(10);
+	}
+
+	int ADC_average = 0;
+	for (int i = 0; i < this->numSamples; i++) {
+		ADC_average += samples[i];
+	}
+	ADC_average /= this->numSamples;
+
 	int adc = analogRead(this->pin);
-	if(this->useBeta) {tempK = this->getTempWithBeta(adc);}
-	else {tempK = this->getTempWithSteinhartHart(adc);}
-
-	tempK = this->getTempWithBeta(tempK);
-
-	//Just illustrating that calling member function with no args works fine
-	double foo = this->getTempF();
+	if(this->useBeta) {tempK = this->getTempWithBeta(ADC_average);}
+	else {tempK = this->getTempWithSteinhartHart(ADC_average);}
 
 	return tempK;
 }
--- SmokerMC/GenericThermistor.h
+++ SmokerMC/GenericThermistor.h
@@ -13,14 +13,21 @@ private:
 
 public:
 	//Constructors
-	GenericThermistor(int pin, int beta, unsigned long seriesRes, unsigned long thermistorNominal, int numSamples=5);
-	GenericThermistor(int pin, float a, float b, float c, unsigned long seriesRes, int numSamples=5);
+	GenericThermistor(int pin, int beta, unsigned long seriesRes, unsigned long thermistorNominalRes,
+			double thermistorNominalTemp, bool isPullUp=false, int numSamples=5);
+	GenericThermistor(int pin, double a, double b, double c, unsigned long seriesRes,
+			bool isPullUp=false, int numSamples=5);
 
 	//Public Variables
-	bool useBeta;
-	int pin, beta, numSamples;
-	unsigned long thermistorNominal, seriesRes;
-	double coeff_A, coeff_B, coeff_C;
+	bool useBeta; //use the Beta equation? If false use the full Steinhart-Hart equation
+	bool isPullUp; //is thermistor tied to Vcc?  If not it is tied to GND and the math is a bit different
+	int pin;  //the analog pin which this resistor is to be read from
+	int beta; //the Beta coefficient, if using the Beta variant of Steinhart-Hart
+	int numSamples; //analog pin will be read this many times and result averaged for data smoothing
+	unsigned long thermistorNominalResistance; //the nominal resistance of the thermistor
+	double thermistorNominalTemperature; //the temperature at which the thermistor's nominal resistance is reported, usually 25C.
+	unsigned long seriesRes; //the value of the "other" resistor used in the voltage divider
+	double coeff_A, coeff_B, coeff_C; //the Steinhart-Hart coefficients
 
 	double getTempC() const;
 	double getTempF() const;
