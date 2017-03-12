cf6325d8f84675ad463dacabfc5609f68c79d826
rober <rober@RKS-M3800>
2017-03-11 19:08:40 -0500

    Fixed Fahrenheit conversion bug

--- SmokerMC/GenericThermistor.cpp
+++ SmokerMC/GenericThermistor.cpp
@@ -9,33 +9,6 @@
 #include <math.h>
 #include "Arduino.h"
 
-GenericThermistor::GenericThermistor(int pin, int beta, unsigned long seriesRes,
-		unsigned long thermistorNominalRes, double thermistorNominalTemp, bool isPullUp, int numSamples,
-		double tempOffset) {
-	this->useBeta = true;
-	this->pin = pin; //analog pin the thermistor is on
-	this->beta = beta; //Beta coefficient of the thermistor
-	this->seriesRes = seriesRes; //value of series resitor in voltage divider
-	this->thermistorNominalResistance = thermistorNominalRes; //the nominal resistance of the thermistor
-	this->thermistorNominalTemperature = thermistorNominalTemp; //the temperature at which the thermistor's nominal resistance is reported, usually 25C.
-	this->isPullUp = isPullUp; //whether or not the thermistor is tied to Vcc (othereise it is pull-down configuration)
-	this->numSamples = numSamples; //for data smoothing, thermistor value will be read this many times and result averaged, default = 5
-	this->tempOffsetK = tempOffset; //plus or minus calibration adjustment to final temp, in Kelvin
-}
-
-GenericThermistor::GenericThermistor(int pin, double a, double b, double c, unsigned long seriesRes,
-		bool isPullUp, int numSamples, double tempOffset) {
-	this->useBeta = false;
-	this->pin = pin; //analog pin the thermistor is on
-	this->coeff_A = a; //Steinhart-Hart coefficient A
-	this->coeff_B = b; //Steinhart-Hart coefficient B
-	this->coeff_C = c; //Steinhart-Hart coefficient C
-	this->seriesRes = seriesRes; //value of series resitor in voltage divider
-	this->isPullUp = isPullUp; //whether or not the thermistor is tied to Vcc (othereise it is pull-down configuration)
-	this->numSamples = numSamples; //for data smoothing, thermistor value will be read this many times and result averaged, default = 5
-	this->tempOffsetK = tempOffset; //plus or minus calibration adjustment to final temp, in Kelvin
-}
-
 //Calculate temperature using the Beta coefficient method
 double GenericThermistor::getTempWithBeta(int ADC_Value) {
 	double temp;
@@ -57,14 +30,26 @@ double GenericThermistor::getTempWithBeta(int ADC_Value) {
 //Calculate temperature using the full Steinhart-Hart equation with 3 coefficients
 double GenericThermistor::getTempWithSteinhartHart(int ADC_Value) {
 	double temp;
+	Serial.println("Inside getTempWithSteinhartHard function. The ADC value passed in is ");
+	Serial.println(ADC_Value);
+
+	Serial.print("Series Reistor is listed at ");
+	Serial.println(this->seriesRes);
 
 	if(this->isPullUp) {
-		temp = log(this->seriesRes / ((1024.0 / ADC_Value - 1))); // for pull-up configuration
+		Serial.println("Using pull-up configuration.");
+		temp = log((this->seriesRes) / ((1024.0 / ADC_Value - 1))); // for pull-up configuration
 	} else {
-		temp = log(this->seriesRes * ((1024.0 / ADC_Value - 1))); // for pull-down configuration
+		Serial.println("Using pull-down configuration.");
+		temp = log((this->seriesRes) * ((1024.0 / ADC_Value - 1))); // for pull-down configuration
 	}
+	Serial.print("Step 1 one of calculation: ");
+	Serial.println(temp);
 
-	temp = 1 / (this->coeff_A + (this->coeff_B + (this->coeff_C * temp * temp ))* temp );
+	temp = 1 / ((this->coeff_A) + ((this->coeff_B) + ((this->coeff_C) * temp * temp ))* temp );
+
+	Serial.print("Returning temp in Kelvin: ");
+	Serial.println(temp);
 
 	return temp;
 }
@@ -85,6 +70,9 @@ double GenericThermistor::getTempK() {
 	}
 	ADC_average /= this->numSamples;
 
+	Serial.print("ADC Average: ");
+	Serial.println(ADC_average);
+
 	if(this->useBeta) {tempK = this->getTempWithBeta(ADC_average);}
 	else {tempK = this->getTempWithSteinhartHart(ADC_average);}
 
@@ -95,15 +83,27 @@ double GenericThermistor::getTempK() {
 
 //Return temperature in Celcius
 double GenericThermistor::getTempC() {
+	Serial.println("Inside getTempC");
+
 	double tempC = this->getTempK();
 	tempC -= 273.15;
+	Serial.print("Returning ");
+	Serial.print(tempC);
+	Serial.println(" Celcius.");
 	return tempC;
 }
 
 //Return temperature in Fahrenheit
 double GenericThermistor::getTempF() {
-	double tempF = this->getTempK();
+	Serial.println("Inside getTempF");
+
+	double tempF = this->getTempC();
+	Serial.print("Converting ");
+	Serial.print(tempF);
+	Serial.print(" Kelvin to ");
 	tempF = (tempF * 9.0) / 5.0 + 32;
+	Serial.print(tempF);
+	Serial.println(" Fahrenheit.");
 	return tempF;
 }
 
